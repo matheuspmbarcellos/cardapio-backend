d4887a06ea11ec3798e210b5c0c8e8979ba71c91
matheuspmbarcellos <matbarcellos21@gmail.com>
2024-06-24 13:02:57 -0300

    feat: config init

---
+++ src/main/java/com/example/cardapio_backend/CardapioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cardapio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardapioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardapioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cardapio_backend/CardapioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cardapio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardapioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
